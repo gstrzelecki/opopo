c88533e2d99e49ef670cba8e0afe75ee0884276b
objectivity\gstrzelecki <gstrzelecki@objectivity.co.uk>
2015-05-11 23:51:43 +0200

    Added sample controller

---
+++ Opopo.Web/Controllers/SampleApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Opopo.Web
+{
+    public class SampleApiController : ApiController
+    {
+        // GET: 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SampleApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SampleApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SampleApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SampleApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
